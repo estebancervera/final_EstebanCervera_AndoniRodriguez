01343d1ad6a25bd80c664e830e156565b60978be
Esteban Cervera <cerverae18@hotmail.com>
2020-11-28 07:48:16 -0600

    sql syntax 2

--- scripts/setup.sql
+++ scripts/setup.sql
@@ -2,15 +2,13 @@
 CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
 
-CREATE DATABASE IF NOT EXISTS `alumno` ;
-USE `alumno`;
+CREATE DATABASE IF NOT EXISTS alumno ;
+USE alumno;
 
-CREATE TABLE IF NOT EXISTS `alumno` (
-  `id` NUMBER NOT NULL,
-  `name` VARCHAR2(50) NOT NULL,
-  `age` NUMBER NOT NULL,
-  `grade` NUMBER NOT NULL,
-  `email` VARCHAR2(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS alumno (
+  id INT  PRIMARY KEY,
+  name VARCHAR(50) ,
+  age INT ,
+  grade INT,
+  email VARCHAR(50)
 );
-ALTER TABLE `alumno`
-  ADD PRIMARY KEY (`id`);
